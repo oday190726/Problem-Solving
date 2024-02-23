5681dc39d200155c7092ee53aac61b260343564e
oday190726 <123557755+oday190726@users.noreply.github.com>
2024-02-23 15:41:58 +0400

    Add files via upload

---
+++ Digits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Digits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        for (int i=0;i<size;i++){
+            int value = scanner.nextInt();
+            if(value == 0){
+                System.out.println(0);
+                continue;
+            }
+            while (value !=0){
+                int mynum = value % 10;
+                System.out.print(mynum + " ");
+                value /= 10;
+            }
+            System.out.println();
+
+        }
+
+    }
+}
+
